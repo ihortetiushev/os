873b7b3a4f93e8e1e2a4278b3f8cfcd4e7afd803
ihortetiushev <ihor.tetiushev@gmail.com>
2023-05-17 18:58:17 +0300

    same here

--- lb4/Receiver/Resource.h
+++ lb4/Receiver/Resource.h
@@ -2,17 +2,21 @@
 // Microsoft Visual C++ generated include file.
 // Used by Receiver.rc
 //
-#define IDR_MAINFRAME					128
-#define IDD_RECEIVER_DIALOG				102
+#define IDD_RECEIVER_DIALOG             102
+#define IDR_MAINFRAME                   128
+#define ID_BTN1                         1000
+#define ID_BTN2                         1001
+#define LABEL1                          1002
+#define CNT2_LABEL                      1003
+#define LABEL2                          1003
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
